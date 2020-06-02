b0fb2a29c3d6fd4bd4d20185a25b4cc45a3167ca
Callum Watkins <callum@callumwatkins.com>
2020-06-02 01:32:21 +0100

    Create TemporalLogParser abstract class
    Represents a parser for log files wth timestamps in each entry. Use template method design pattern.

--- AWSLogMerger/LogParser.cs
+++ AWSLogMerger/LogParser.cs
@@ -6,5 +6,11 @@ namespace AWSLogMerger
     internal abstract class LogParser
     {
         public abstract IEnumerable<string> Parse(ICollection<string> fileNames);
+
+        protected abstract IEntryEnumerator GetEntryEnumerator(string file);
+
+        protected interface IEntryEnumerator : IEnumerable<string>, IDisposable
+        {
+        }
     }
 }
---
+++ AWSLogMerger/TemporalLogParser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AWSLogMerger
+{
+    internal abstract class TemporalLogParser : LogParser
+    {
+        public override IEnumerable<string> Parse(ICollection<string> fileNames)
+        {
+            if (fileNames.Count == 0) throw new ParseException("Source directory contains no files.");
+
+            var lines = new ConcurrentBag<(DateTime dateTime, string entry)>();
+
+            Parallel.ForEach(fileNames, file =>
+            {
+                using IEntryEnumerator entryEnumerator = GetEntryEnumerator(file);
+                foreach (string entry in entryEnumerator)
+                    lines.Add((ExtractDateTime(entry), entry));
+            });
+
+            return lines.AsParallel()
+                .OrderBy(entry => entry.dateTime)
+                .Select(entry => entry.entry);
+        }
+
+        protected abstract DateTime ExtractDateTime(string entry);
+    }
+}
