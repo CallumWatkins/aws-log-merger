8dbcf1cdd4f9fd8695d34d6e7f3b919eb8a6127f
Callum Watkins <callum@callumwatkins.com>
2020-06-03 00:42:44 +0100

    Implement FileLogWriter class

---
+++ AWSLogMerger/FileLogWriter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace AWSLogMerger
+{
+    internal sealed class FileLogWriter : LogWriter
+    {
+        private readonly bool _overwrite;
+
+        public FileLogWriter(string basePath, bool overwrite) : base(basePath)
+        {
+            _overwrite = overwrite;
+        }
+
+        public override bool SupportsParallelWriting => true;
+
+        public override void Write(string name, IEnumerable<string> content)
+        {
+            string path = Path.Combine(NamePrefix, name);
+
+            if (!_overwrite && File.Exists(path)) throw new IOException($"File already exists: '{path}'");
+
+            Stream file = File.OpenWrite(path);
+            using var sw = new StreamWriter(file);
+
+            foreach (var line in content)
+                sw.WriteLine(line);
+        }
+    }
+}
