edee99988991179e06a59f0818f4744fdcbe4bc2
Callum Watkins <callum@callumwatkins.com>
2020-06-02 01:24:54 +0100

    Create LogParser abstract class

---
+++ AWSLogMerger/LogParser.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AWSLogMerger
+{
+    internal abstract class LogParser
+    {
+        public abstract IEnumerable<string> Parse(ICollection<string> fileNames);
+    }
+}
