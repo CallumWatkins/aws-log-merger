f6be4fbc2ddcd9391fbcb4ad5a9970e6fac1c3b4
Callum Watkins <callum@callumwatkins.com>
2020-06-03 00:24:59 +0100

    Create LogWriter abstract class

---
+++ AWSLogMerger/LogWriter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace AWSLogMerger
+{
+    internal abstract class LogWriter
+    {
+        public LogWriter(string namePrefix)
+        {
+            NamePrefix = namePrefix;
+        }
+
+        public abstract bool SupportsParallelWriting { get; }
+
+        public string NamePrefix { get; }
+
+        public abstract void Write(string name, IEnumerable<string> content);
+    }
+}
