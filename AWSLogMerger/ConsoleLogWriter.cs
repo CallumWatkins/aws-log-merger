9e636d669f49c0f4ed5604ca6d26140391a1eaa9
Callum Watkins <callum@callumwatkins.com>
2020-06-03 00:25:23 +0100

    Implement ConsoleLogWriter class

---
+++ AWSLogMerger/ConsoleLogWriter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AWSLogMerger
+{
+    internal sealed class ConsoleLogWriter : LogWriter
+    {
+        public ConsoleLogWriter(string namePrefix) : base(namePrefix)
+        {
+        }
+
+        public override bool SupportsParallelWriting => false;
+
+        public override void Write(string name, IEnumerable<string> content)
+        {
+            Console.WriteLine("");
+            Console.WriteLine($"Output log '{Path.Combine(NamePrefix, name)}':");
+            foreach (string line in content)
+                Console.WriteLine(line);
+        }
+    }
+}
